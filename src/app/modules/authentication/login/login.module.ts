f8268b5aa30084dc70686051f35607d6f54f0f6a
7zgui <ouokki98@gmail.com>
2022-12-14 19:18:35 +0100

    generate components

--- src/app/modules/authentication/login/login.module.ts
+++ src/app/modules/authentication/login/login.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogingRoutingModule } from './loging-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     LogingRoutingModule
