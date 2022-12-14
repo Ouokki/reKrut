10c24777eb5cbf762c417bb932a96b7f5643b090
7zgui <ouokki98@gmail.com>
2022-12-14 19:14:39 +0100

    structuring

---
+++ src/app/modules/authentication/login/login.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LogingRoutingModule } from './loging-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LogingRoutingModule
+  ]
+})
+export class LoginModule { }
